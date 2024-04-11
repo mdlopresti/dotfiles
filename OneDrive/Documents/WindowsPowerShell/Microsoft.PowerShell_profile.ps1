abcdb9311f1f63cb6571838d5f7a8247efab14f7
Mike LoPresti <mike@lopresti.org>
2024-04-11 08:07:03 -0700

    general updates

--- OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,5 +4,8 @@ function lpass {
 function lpass-add-ssh($KeyName) {
   wsl printf "Private Key:$(cat .ssh/$KeyName)\nPublic Key:$(cat .ssh/$KeyName.pub)" `| lpass add --sync=now --non-interactive --note-type=ssh-key $KeyName
 }
+if($null -ne $(Get-Command kubectl -ErrorAction SilentlyContinue)) {
+  kubectl completion powershell | Out-String | Invoke-Expression
+}
 
 oh-my-posh init pwsh --config ($env:USERPROFILE + "\.oh-my-posh.omp.json") | Invoke-Expression
