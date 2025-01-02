3ea92b916ef47d6b6ee97caee617c2631c47e1c1
Michael LoPresti <mdl4071@yum.com>
2025-01-02 08:33:11 -0800

    expanding setup for lmde

--- .chezmoiscripts/linux/run_onchange_install-shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O ~/oh-my-posh
-chmod +x ~/oh-my-posh
-
-echo 'eval "$(~/oh-my-posh --init --shell bash --config ~/.oh-my-posh.omp.json)"' >> ~/.bashrc
